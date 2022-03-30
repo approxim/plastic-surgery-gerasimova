1c0c71f7fd7c81c3078a611f830860ae74158646
DmitrySergeevich <efos.ds@gmail.com>
2022-03-30 13:34:54 +0700

    налажена работа меню и исправлены косяки в aboutme

--- source/js/app.js
+++ source/js/app.js
@@ -91,3 +91,21 @@ $(document).ready(function(){
   });
 })
 
+
+// Плавная прокрутка до элемента
+$("[data-scroll]").on("click", function(event) {
+  event.preventDefault();
+
+  var $this = $(this),
+    blockId = $this.data('scroll'),
+    blockOffset = $(blockId).offset().top;
+
+  $("#nav a").removeClass("active");
+  $this.addClass("active");
+
+
+  $("html, body").animate({
+    scrollTop: blockOffset -30 // отступ
+  }, 500);
+
+});
