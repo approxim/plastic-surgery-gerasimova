f2f60a8369e8959e080e391e609d599e20c7b0c5
DmitrySergeevich <efos.ds@gmail.com>
2022-04-08 13:12:57 +0700

    плавное появление и исчезновение модальника

--- source/js/app.js
+++ source/js/app.js
@@ -14,140 +14,6 @@ $(".faq__title").on("click", function () {
 $("#feedbacktel").mask("+7(999) 999-9999");
 $("#feedbacktel-modal").mask("+7(999) 999-9999");
 
-
-// Кнопка выбора опции (тело-лицо)
-let operationButton = document.querySelector(".operation__select");
-let operationButtonText = document.querySelector(".operation__select-label");
-let operationSorter = document.querySelector(".operation__sorter");
-
-const NODES = ["BUTTON", "LABEL"];
-
-window.addEventListener("click", e => {
-    if (NODES.includes(e.target.nodeName)) return;
-    operationButton.classList.remove("active");
-});
-
-operationButton.addEventListener("click", e => {
-    operationButton.classList.toggle("active");
-});
-
-operationSorter.addEventListener("click", e => {
-    operationButtonText.innerText = e.target.innerText;
-    operationButton.classList.add("selected");
-    operationButton.classList.remove("active");
-});
-
-// Табы в секции clinic (кнопки абакан-красноярск)
-function openPage(pageName, elmnt) {
-
-    var i, allClinicCityContent, elmnt;
-    allClinicCityContent = document.getElementsByClassName("clinic__inner");
-
-    for (i = 0; i < allClinicCityContent.length; i++) {
-        allClinicCityContent[i].style.visibility = "hidden";
-        allClinicCityContent[i].style.height = "0";
-    }
-
-    document.getElementById(pageName).style.visibility = "visible";
-    document.getElementById(pageName).style.height = "auto";
-
-    //Получаем все кнопки и удаляем класс "Эктив"
-    let allClinicCityButtons = document.getElementsByClassName("clinic__tab");
-    for (i = 0; i < allClinicCityButtons.length; i++) {
-        allClinicCityButtons[i].className = allClinicCityButtons[i].className.replace(" active", "");
-    }
-    // даем класс эктив нажатой кнопке
-    elmnt.className += " active";
-}
-// Делаем активной вкладку "Красноярск"
-document.querySelector(".clinic__sorter-item").click();
-
-
-
-// Табы в модальном окне Operations (Result)
-function openResult(pageName, elmnt) {
-
-    var i, modalSliderResult, elmnt;
-    modalSliderResult = document.getElementsByClassName("modal-result__slides");
-
-    for (i = 0; i < modalSliderResult.length; i++) {
-        modalSliderResult[i].style.visibility = "hidden";
-        modalSliderResult[i].style.height = "0";
-    }
-
-    document.getElementById(pageName).style.visibility = "visible";
-    document.getElementById(pageName).style.height = "auto";
-
-    //Получаем все кнопки и удаляем класс "Эктив"
-    let allResultButtons = document.getElementsByClassName("modal-result__tab");
-    for (i = 0; i < allResultButtons.length; i++) {
-        allResultButtons[i].className = allResultButtons[i].className.replace(" active", "");
-    }
-    // даем класс эктив нажатой кнопке
-    elmnt.className += " active";
-}
-// Кликаем по кнопке слайдера Result
-document.querySelector("modal-result__tab")
-
-
-
-// отображение элементов Операции (тело-лицо)
-$(document).ready(function () {
-    $('.operation__sorter-item').on('click', function () {
-        var value = $(this).attr("data-filter");
-
-        if (window.innerWidth > 767) {
-            var value = $(this).attr("data-filter");
-            var elem = $(".operations__slide");
-            if (value == "all") {
-                $(elem).show("500");
-            } else {
-                $(elem).not("." + value).hide("500");
-                $(elem).filter("." + value).show("500");
-            }
-        } else {
-            if (value === 'all') {
-                $('.operations__wrapper').slick('slickUnfilter');
-                $('.operations__wrapper').slick('slickFilter', '.all');
-
-            } else {
-                if (value === 'body') {
-                    $('.operations__wrapper').slick('slickUnfilter');
-                    $('.operations__wrapper').slick('slickFilter', '.body');
-                } else {
-                    $('.operations__wrapper').slick('slickUnfilter');
-                    $('.operations__wrapper').slick('slickFilter', '.face');
-                }
-            }
-        }
-    });
-})
-
-// Кнопка выбора города
-let clinicButton = document.querySelector(".clinic__select");
-let clinicButtonText = document.querySelector(".clinic__select-label");
-let clinicSorter = document.querySelector(".clinic__sorter");
-let clinicFirstButton = document.querySelector(".clinic__sorter-item"); // первый попавшийся таб
-
-const CLINICNODES = ["BUTTON", "LABEL"];
-
-clinicButtonText.innerText = clinicFirstButton.innerText;
-
-window.addEventListener("click", e => {
-    if (CLINICNODES.includes(e.target.nodeName)) return;
-    clinicButton.classList.remove("active");
-});
-
-clinicButton.addEventListener("click", e => {
-    clinicButton.classList.toggle("active");
-});
-
-clinicSorter.addEventListener("click", e => {
-    clinicButtonText.innerText = e.target.innerText;
-    clinicButton.classList.add("selected");
-    clinicButton.classList.remove("active");
-});
-
 // Плавная прокрутка до элемента
 $("[data-scroll]").on("click", function (event) {
     event.preventDefault();
@@ -166,10 +32,11 @@ $("[data-scroll]").on("click", function (event) {
 });
 
 
-// Запуск модалки "Запись на консультацию"
+// // Запуск модалки "Запись на консультацию"
 $(".to-appointment").on("click", function () {
     document.body.style.overflow = 'hidden';
-    $("#modal__form").css("display", "flex").hide().fadeIn('fast'); //плавно присваивает FLEX
+    $("#modal__form").addClass("active");
+    // .hide().fadeIn('fast'); //плавно присваивает FLEX
     // Закрытие модалки "Запись на консультацию"
     $(document).mouseup(function (e) { // событие клика по веб-документу
         var modalForm = $("#modal-feedback__form"); // сама форма
@@ -177,12 +44,14 @@ $(".to-appointment").on("click", function () {
 
         $("#modal-form__close").on("click", function () {
             document.body.style.overflow = 'scroll';
-            $("#modal__form").hide('fast');//скрывает див модалки при клике на кнопку закрытия
+            $("#modal__form").removeClass("active");
+            // .hide('fast');//скрывает див модалки при клике на кнопку закрытия
         });
 
         if (!modalForm.is(e.target) // если клик был не по нашему блоку
             && modalForm.has(e.target).length === 0) { // и не по его дочерним элементам
-            modalWindow.hide('fast'); // скрываем его
+            modalWindow.removeClass("active");
+            // .hide('fast'); // скрываем его
             document.body.style.overflow = 'scroll';
         }
     });
---
+++ source/js/tab-button-choise.js
@@ -0,0 +1,131 @@
+// Кнопка выбора опции (тело-лицо)
+let operationButton = document.querySelector(".operation__select");
+let operationButtonText = document.querySelector(".operation__select-label");
+let operationSorter = document.querySelector(".operation__sorter");
+
+const NODES = ["BUTTON", "LABEL"];
+
+window.addEventListener("click", e => {
+    if (NODES.includes(e.target.nodeName)) return;
+    operationButton.classList.remove("active");
+});
+
+operationButton.addEventListener("click", e => {
+    operationButton.classList.toggle("active");
+});
+
+operationSorter.addEventListener("click", e => {
+    operationButtonText.innerText = e.target.innerText;
+    operationButton.classList.add("selected");
+    operationButton.classList.remove("active");
+});
+
+// Табы в секции clinic (кнопки абакан-красноярск)
+function openPage(pageName, elmnt) {
+
+    var i, allClinicCityContent, elmnt;
+    allClinicCityContent = document.getElementsByClassName("clinic__inner");
+
+    for (i = 0; i < allClinicCityContent.length; i++) {
+        allClinicCityContent[i].style.visibility = "hidden";
+        allClinicCityContent[i].style.height = "0";
+    }
+
+    document.getElementById(pageName).style.visibility = "visible";
+    document.getElementById(pageName).style.height = "auto";
+
+    //Получаем все кнопки и удаляем класс "Эктив"
+    let allClinicCityButtons = document.getElementsByClassName("clinic__tab");
+    for (i = 0; i < allClinicCityButtons.length; i++) {
+        allClinicCityButtons[i].className = allClinicCityButtons[i].className.replace(" active", "");
+    }
+    // даем класс эктив нажатой кнопке
+    elmnt.className += " active";
+}
+// Делаем активной вкладку "Красноярск"
+document.querySelector(".clinic__sorter-item").click();
+
+
+// Табы в модальном окне Operations (Result)
+function openResult(pageName, elmnt) {
+
+    var i, modalSliderResult, elmnt;
+    modalSliderResult = document.getElementsByClassName("modal-result__slides");
+
+    for (i = 0; i < modalSliderResult.length; i++) {
+        modalSliderResult[i].style.visibility = "hidden";
+        modalSliderResult[i].style.height = "0";
+    }
+
+    document.getElementById(pageName).style.visibility = "visible";
+    document.getElementById(pageName).style.height = "auto";
+
+    //Получаем все кнопки и удаляем класс "Эктив"
+    let allResultButtons = document.getElementsByClassName("modal-result__tab");
+    for (i = 0; i < allResultButtons.length; i++) {
+        allResultButtons[i].className = allResultButtons[i].className.replace(" active", "");
+    }
+    // даем класс эктив нажатой кнопке
+    elmnt.className += " active";
+}
+// Кликаем по кнопке слайдера Result
+document.querySelector("modal-result__tab")
+
+
+
+// отображение элементов Операции (тело-лицо)
+$(document).ready(function () {
+    $('.operation__sorter-item').on('click', function () {
+        var value = $(this).attr("data-filter");
+
+        if (window.innerWidth > 767) {
+            var value = $(this).attr("data-filter");
+            var elem = $(".operations__slide");
+            if (value == "all") {
+                $(elem).show("500");
+            } else {
+                $(elem).not("." + value).hide("500");
+                $(elem).filter("." + value).show("500");
+            }
+        } else {
+            if (value === 'all') {
+                $('.operations__wrapper').slick('slickUnfilter');
+                $('.operations__wrapper').slick('slickFilter', '.all');
+
+            } else {
+                if (value === 'body') {
+                    $('.operations__wrapper').slick('slickUnfilter');
+                    $('.operations__wrapper').slick('slickFilter', '.body');
+                } else {
+                    $('.operations__wrapper').slick('slickUnfilter');
+                    $('.operations__wrapper').slick('slickFilter', '.face');
+                }
+            }
+        }
+    });
+})
+
+// Кнопка выбора города
+let clinicButton = document.querySelector(".clinic__select");
+let clinicButtonText = document.querySelector(".clinic__select-label");
+let clinicSorter = document.querySelector(".clinic__sorter");
+let clinicFirstButton = document.querySelector(".clinic__sorter-item"); // первый попавшийся таб
+
+const CLINICNODES = ["BUTTON", "LABEL"];
+
+clinicButtonText.innerText = clinicFirstButton.innerText;
+
+window.addEventListener("click", e => {
+    if (CLINICNODES.includes(e.target.nodeName)) return;
+    clinicButton.classList.remove("active");
+});
+
+clinicButton.addEventListener("click", e => {
+    clinicButton.classList.toggle("active");
+});
+
+clinicSorter.addEventListener("click", e => {
+    clinicButtonText.innerText = e.target.innerText;
+    clinicButton.classList.add("selected");
+    clinicButton.classList.remove("active");
+});
